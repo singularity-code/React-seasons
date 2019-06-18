3ef2eb67904688ff65d5e6d52e795959cc7b9127
Chris <neoxrock@gmail.com>
2019-06-18 17:08:30 +1000

    Spinner

---
+++ src/Spinner.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Spinner = () => {
+  return (
+    <div className="ui active inverted dimmer">
+      <div className="ui massive text loader">Loading...</div>
+    </div>
+  );
+};
+
+export default Spinner;
