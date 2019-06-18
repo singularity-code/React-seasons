8923714f16c11dd93d459e60ad1166cbde9e5955
Chris <neoxrock@gmail.com>
2019-06-18 16:43:26 +1000

    Icons

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -10,11 +10,16 @@ const getSeason = (lat, month) => {
 
 const SeasonDisplay = (props) => {
   const season = getSeason(props.lat, new Date().getMonth());
+  const text = season === 'winter' ? 'Burr, it is chilly' : 'Lets go to the beach';
+  const icon = season === 'wintet' ? 'snowflake' : 'sun';
 
-  console.log(season);
-
-  console.log(props.lat);
-  return <div>Season Display</div>;
+  return (
+    <div>
+      <i className={`${icon} icon`} />
+      <h1>{text}</h1>
+      <i className={`${icon} icon`} />
+    </div>
+  )
 };
 
 export default SeasonDisplay;
