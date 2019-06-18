b213070cdc7751d4aad0d4d40463a77bf03dcbd7
Chris <neoxrock@gmail.com>
2019-06-18 15:40:51 +1000

    Conditionally Rendering

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = { lat: null, errorMessage: ''};
-    
+
     window.navigator.geolocation.getCurrentPosition(
       (position) => {
         this.setState({
@@ -20,13 +20,15 @@ class App extends React.Component {
     );
   }
   render() {
-    return (
-      <div>
-        Latitude: {this.state.lat}
-        <br/>
-        Error: {this.state.errorMessage}
-      </div>
-    );
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div> Error: {this.state.errorMessage}</div>
+    }
+
+    if (!this.state.errorMessage && this.state.lat) {
+      return <div>latitude: {this.state.lat} </div>
+    }
+
+    return <div>Loading!</div>
   }
 }
 
