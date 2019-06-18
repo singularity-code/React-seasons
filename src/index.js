9c53c69fe8c1bf936cd2770605cd3befb894e846
Chris <neoxrock@gmail.com>
2019-06-18 16:00:39 +1000

    Life Cycle methods

--- src/index.js
+++ src/index.js
@@ -19,6 +19,15 @@ class App extends React.Component {
       }
     );
   }
+
+  componentDidMount() {
+    console.log("My component was rendered to the screen");
+  }
+
+  componentDidUpdate() {
+    console.log("My component was just updated");
+  }
+
   render() {
     if (this.state.errorMessage && !this.state.lat) {
       return <div> Error: {this.state.errorMessage}</div>
